59e917316affb55c1893c3fff4e1edad54418896
Amey Kusurkar <amey1000@gmail.com>
2022-03-21 23:02:04 +0000

    Add starship config

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,6 @@
 set -gx DOTFILES $HOME/dotfiles
 set -gx OMF_CONFIG $DOTFILES/omf
+set -gx STARSHIP_CONFIG $DOTFILES/starship.toml
 
 abbr -a gd git diff
 abbr -a ga git add
@@ -8,8 +9,17 @@ abbr -a gl git pull
 abbr -a gp git push
 abbr -a gc git commit
 abbr -a gcam git commit -a -m
+abbr -a glg git log --stat
+abbr -a gcaa git commit -a --amend
 
 abbr -a sfish source ~/.config/fish/config.fish
+
 abbr -a vfish vim ~/.config/fish/config.fish
+abbr -a vstar vim $STARSHIP_CONFIG
 
 abbr -a vimrc vim ~/.vimrc
+
+[ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
+
+# Needs to be the last line of the config
+starship init fish | source
