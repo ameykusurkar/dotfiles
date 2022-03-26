dd841bfd73fea126e6f3c19d154e84c92a8e55f5
Amey Kusurkar <amey1000@gmail.com>
2022-03-26 08:45:34 +0000

    Add some abbreviations to config

--- fish/config.fish
+++ fish/config.fish
@@ -17,6 +17,9 @@ abbr -a grbi git rebase -i
 abbr -a grba git rebase --abort
 abbr -a grbc git rebase --continue
 
+abbr -a be bundle exec
+abbr -a dcopa 'git diff origin/master --name-only --relative --diff-filter=ACMRTUXB | grep ".*.rb\$" | xargs bundle exec rubocop -a'
+
 abbr -a sfish source ~/.config/fish/config.fish
 
 abbr -a vfish vim ~/.config/fish/config.fish
@@ -24,6 +27,9 @@ abbr -a vstar vim $STARSHIP_CONFIG
 
 abbr -a vimrc vim ~/.vimrc
 abbr -a penv vim ~/.private-env.fish
+
+abbr -a zshrc vim ~/.zshrc
+abbr -a penvz vim ~/.private-env
 
 [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
 
