14bbfa394bfe33af775618474a82898ba23e345d
Amey Kusurkar <amey1000@gmail.com>
2022-03-27 16:06:42 +0100

    Add and sort git abbrs

--- fish/config.fish
+++ fish/config.fish
@@ -7,20 +7,21 @@ set -gx GOPATH (go env GOPATH)
 fish_add_path -g $HOME/.cargo/bin
 fish_add_path -g $GOPATH/bin
 
-abbr -a gd git diff
 abbr -a ga git add
-abbr -a gst git status
-abbr -a gl git pull
-abbr -a gp git push
+abbr -a gb git branch
 abbr -a gc git commit
-abbr -a gcam git commit -a -m
-abbr -a glg git log --stat
 abbr -a gcaa git commit -a --amend
-abbr -a gco git checkout
+abbr -a gcam git commit -a -m
 abbr -a gcb git checkout -b
-abbr -a grbi git rebase -i
+abbr -a gco git checkout
+abbr -a gd git diff
+abbr -a gl git pull
+abbr -a glg git log --stat
+abbr -a gp git push
 abbr -a grba git rebase --abort
 abbr -a grbc git rebase --continue
+abbr -a grbi git rebase -i
+abbr -a gst git status
 
 abbr -a be bundle exec
 abbr -a dcopa 'git diff origin/master --name-only --relative --diff-filter=ACMRTUXB | grep ".*.rb\$" | xargs bundle exec rubocop -a'
