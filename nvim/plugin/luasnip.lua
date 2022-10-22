e2f3d00f35357565a2389d48fc206657b3bd2141
Amey Kusurkar <amey1000@gmail.com>
2022-10-22 16:50:09 +0100

    Add some ruby snippets

--- nvim/plugin/luasnip.lua
+++ nvim/plugin/luasnip.lua
@@ -3,3 +3,15 @@ local ls = require('luasnip')
 ls.add_snippets("lua", {
   ls.parser.parse_snippet("fu", "function $1($2)\n  $0\nend", {}),
 })
+
+ls.add_snippets("ruby", {
+  ls.parser.parse_snippet("if", "if $1\n  $0\nend", {}),
+
+  ls.parser.parse_snippet("map", "map { |$1| $0 }", {}),
+  ls.parser.parse_snippet("mapd", "map do |$1|\n  $0\nend", {}),
+
+  ls.parser.parse_snippet("let", "let(:$1) { $0 }", {}),
+  ls.parser.parse_snippet("let!", "let!(:$1) { $0 }", {}),
+  ls.parser.parse_snippet("letd", "let(:$1) do \n  $0\nend", {}),
+  ls.parser.parse_snippet("let!d", "let!(:$1) do \n  $0\nend", {}),
+})
