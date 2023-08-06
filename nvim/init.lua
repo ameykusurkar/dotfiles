37182e703e5ef1d53c8d6e6045d71088b8618d25
Amey Kusurkar <amey1000@gmail.com>
2023-08-06 19:22:03 +0100

    Small improvements

--- nvim/init.lua
+++ nvim/init.lua
@@ -268,6 +268,10 @@ vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { de
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 
+vim.keymap.set('n', '<leader>sc', function()
+  require('telescope.builtin').find_files({ cwd = "$DOTFILES" })
+end, { desc = '[S]earch [C]onfig files' })
+
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
