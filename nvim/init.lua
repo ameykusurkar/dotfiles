2b9b58a6298097eaf33f6a503d364bd2c043179a
Amey Kusurkar <amey1000@gmail.com>
2023-08-07 09:56:57 +0100

    Ignore current buffer when showing open buffers

--- nvim/init.lua
+++ nvim/init.lua
@@ -256,7 +256,10 @@ pcall(require('telescope').load_extension, 'fzf')
 
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader><space>', function()
+  require('telescope.builtin').buffers({ ignore_current_buffer = true })
+end, { desc = '[ ] Find existing buffers' })
+
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
