9a4316ea34346040e83f81d5dacf811f619069ad
Amey Kusurkar <amey1000@gmail.com>
2024-04-10 09:29:36 +0100

    Update lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -425,7 +425,9 @@ end
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  gopls = {},
+  gopls = {
+    gofumpt = true,
+  },
   sorbet = {},
   rust_analyzer = {},
   hls = {},
