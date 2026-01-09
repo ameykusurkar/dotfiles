3d2beb6b19d67bf79c77340552ce2c2340d99306
Amey Kusurkar <amey1000@gmail.com>
2026-01-09 14:10:39 +0000

    Make cmdline output visible
    Inspired from: https://github.com/folke/noice.nvim/issues/1097

--- nvim-lazy/lua/plugins/noice.lua
+++ nvim-lazy/lua/plugins/noice.lua
@@ -4,5 +4,15 @@ return {
     cmdline = {
       view = "cmdline",
     },
+    -- Fix to make cmdline output visible
+    routes = {
+      {
+        filter = {
+          event = "msg_show",
+          kind = { "shell_out", "shell_err" },
+        },
+        view = "popup",
+      },
+    },
   },
 }
